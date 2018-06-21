63c745aa1a442a7f83cdc4782137898bcaa52a62
Piotr <spitfire182@users.noreply.github.com>
2018-06-21 01:23:42 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -71,6 +71,28 @@ $(document).ready(function () {
         offset: '70%'
     });
 
+    
+    $('.counters').waypoint(function (direction) {
+        
+        $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+        
+        
+
+}, {
+        offset: '30%'
+    });
+    
+    
 
     /* Mobile nav */
     $(".js--nav-icon").click(function () {
